843f38f18f627697754524679eeca4043b09c5f1
devonwebbR2K <devonwebb559@gmail.com>
2023-01-09 18:46:40 -0800

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use miscmath::prelude::*;
 use raylib::prelude::*;
 
 fn main() {
-
-
+    
     const SCREEN_SIZE: ( i32, i32 ) = ( 640, 480 );
     let ( width, height ) = ( SCREEN_SIZE.0 as f32, SCREEN_SIZE.1 as f32 );
 
@@ -12,4 +10,13 @@ fn main() {
             .title("rusty_nature_of_code")
             .build();
     
+    rl.set_target_fps( 60 );
+    
+    while !rl.window_should_close( ) {
+        
+        let mut display = rl.begin_drawing( &thread );
+        display.clear_background( Color::WHITE );
+        
+        
+    }
 }
