7aa49a82166b447bc7643fa8365ede65c3e8d0f1
devonwebbR2K <devonwebb559@gmail.com>
2023-01-10 02:27:53 -0800

    Included my ecs system misc_ecs and created a bouncy ball simulation with it

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 //! # Rusty Nature of Code
 //!
-//! `rusty_nature_of_code` is a project using rust, with the "RayLib" library and my own math library, miscmath, to follow along with Daniel Shiffman's "The Nature of Code" book
+//! `rusty_nature_of_code` is a project using rust, with the "RayLib" library and my libraries, miscmath and misc_ecs, to follow along with Daniel Shiffman's "The Nature of Code" book
 //! and his "The Nature of Code 2" video series on his YouTube channel "The Coding Train".
 
 pub mod prelude;
