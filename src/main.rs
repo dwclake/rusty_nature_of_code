af0abc3104ffdd31e49d35b8dca46979497f01b5
devonwebbR2K <devonwebb559@gmail.com>
2023-01-09 18:36:46 -0800

    initial commit

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,9 @@ fn main() {
     const SCREEN_SIZE: ( i32, i32 ) = ( 640, 480 );
     let ( width, height ) = ( SCREEN_SIZE.0 as f32, SCREEN_SIZE.1 as f32 );
 
-    let x = Vec2::new();
+    let ( mut rl, thread ) = init( )
+            .size(SCREEN_SIZE.0, SCREEN_SIZE.1)
+            .title("rusty_nature_of_code")
+            .build();
+    
 }
