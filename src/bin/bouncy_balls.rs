70eddfbf465a3989ee1b2fe8e770c0c68fd2471e
devonwebbR2K <devonwebb559@gmail.com>
2023-01-11 00:40:45 -0800

    Added a bin directory and moved main.rs in that directory, then renamed it bouncy_balls.rs. This allows for making many different examples in this project which each can be run separately. Also made variable names more clear and added more documentation

--- src/bin/bouncy_balls.rs
+++ src/bin/bouncy_balls.rs
@@ -52,29 +52,24 @@ fn main() {
         
         /* Creation of the RayLib draw handle. Drawing functions are members of this object, so must be called from this object */
         let mut display = rl.begin_drawing( &thread );
-        
         /* Clears the background and sets it's colour to white */
         display.clear_background( Color::WHITE );
     
         /* Creates entities until there are 10 entities active */
         if world.entity_manager().len() < 10 {
-            
+    
+            /* Creates a new entity id */
+            let entity = world.entity_manager_mut().next();
             /* Generates a random color */
             let color = Color::new( thread_rng().gen_range(100..255),
                                     thread_rng().gen_range(100..255),
                                     thread_rng().gen_range(100..255), 255);
-            /* Creates a new entity id */
-            let entity = world.entity_manager_mut().next();
-            
             /* Adds the entity with a attribute component, which is the same for every entity in this case */
             atr_store.add(entity, Attributes { mass: 1.3, radius: 10.0, color, region: 0 } );
-            
             /* Add the entity with a random position vector, from x: 0.0 to screen width, y: 300.0 to screen height */
             pos_store.add(entity, Vec2::create_random2(&(0.0..width), &(300.0..height) ) );
-            
             /* Adds the entity with a random velocity vector with a angle from pi (180) to tau (360) and a magnitude of 5.0 */
             vel_store.add(entity, Vec2::from_angle(&thread_rng().gen_range( PI..TAU ), &Some(5.0) ) );
-            
             /* Adds the entity with a acceleration vector with x and y at 0.0 */
             acc_store.add(entity, Vec2::new() );
         }
@@ -86,7 +81,6 @@ fn main() {
     
         /* Runs the acceleration system, which adds the velocity components to the corresponding acceleration components */
         acceleration_system(&mut acc_store, &mut vel_store);
-        
         /* Constrains all the velocities from -25.0 to 25.0 */
         vel_store.for_each_mut( |_, v| {
             v.constrain( &(-25.0..25.0), &(-25.0..25.0) );
@@ -94,18 +88,14 @@ fn main() {
         /* Runs the collision system, which checks for collisions in the entities current region, then swaps their directions
            Not properly detecting the collisions yet though */
         collision_system(&mut vel_store, &pos_store, &mut atr_store, &mut regions );
-        
         /* Runs the movement system which moves applies the velocity to the position vectors,
            then calculates what region they are currently in */
         movement_system(SCREEN_SIZE, &mut vel_store, &mut pos_store, &mut atr_store, &mut regions );
-        
         /* Runs the boundary system which checks if the entity has reached the edges of the screen, if they have their velocities are inverted
            and reduced based on their mass. This makes the entities bounce off of surfaces. Also limits their positions to the screen */
         boundary_system(SCREEN_SIZE, &mut vel_store, &mut pos_store, &atr_store);
-        
         /* Runs the render system which draws the entities at their positions as circles */
         render_system(&mut display, SCREEN_SIZE, &pos_store, &atr_store);
-        
         /* Runs the drop system, which removes entities. Currently only removes them when they go out of bounds so only has effect if
            boundary system is commented out */
         drop_system(SCREEN_SIZE, &mut world.entity_manager_mut(),
@@ -115,15 +105,12 @@ fn main() {
         let x = format!( "Pass = {}", pass );
         display.draw_text( &x, 12, 12, 20, Color::BLACK );
         pass += 1;
-    
         /* Draws the FPS to the top left of the screen */
         let x = format!( "FPS = {}", display.get_fps() );
         display.draw_text( &x, 12, 32, 20, Color::BLACK );
     }
-    
     /* Prints out the active entities which has position components when the screen was closed */
     for (entity, p) in pos_store {
         println!( "{:?}: {:?}", entity, p);
     }
-    
 }
--- src/noc/systems.rs
+++ src/noc/systems.rs
@@ -26,35 +26,30 @@ pub fn movement_system< V: Store<Vec2>, P: Store<Vec2>, A: Store<Attributes> >(s
 		/* Gets the attribute component for the current entity */
 		let atr = atr_store.get_mut( entity ).unwrap();
 		
-		// If current entity has a velocity component, assign it to vel and run the following code block
+		/* If current entity has a velocity component, assign it to vel and run the following code block */
 		if let Some( vel ) = vel_store.get_mut( entity ) {
 			
-			// Adds the velocity component to the position component
+			/* Adds the velocity component to the position component */
 			pos.add( vel );
 			
-			// Removes the current entity from it's current region (it may still be in this region and will be
-			// added back or it may have moved to a new region)
+			/* Removes the current entity from it's current region (it may still be in this region and will be
+			   added back or it may have moved to a new region) */
 			regions[atr.region].remove( &entity.id() );
-			
-			// Calculates the column number of the current region or grid and clamps that value to the
-			// max number of columns based on the number of regions
+			/* Calculates the column number of the current region or grid and clamps that value to the
+			   max number of columns based on the number of regions */
 			let mut col = ( (pos.x / screen_size.0) * 10.0 ) as usize;
 			col = col.clamp( 1, regions.len() / ( regions.len() / 4 ) + 1 );
-			
-			// Calculates the row number of the current region or grid and clamps that value to the
-			// max number of rows based on the number of regions
+			/* Calculates the row number of the current region or grid and clamps that value to the
+			   max number of rows based on the number of regions */
 			let mut row = ( (pos.y / screen_size.1) * 10.0 ) as usize;
 			row = row.clamp( 1, regions.len() / ( regions.len() / 4 ) + 1 );
-			
-			// Uses the row and column to calculate the index number for that region in the vector of regions and
-			// clamps the value so it can't go out of bounds
+			/* Uses the row and column to calculate the index number for that region in the vector of regions and
+			   clamps the value so it can't go out of bounds */
 			let mut region_index: usize = col * row - 1;
 			region_index = region_index.clamp( 0, regions.len() - 1 );
-			
-			// Assigns the entities new region
+			/* Assigns the entities new region */
 			atr.region = region_index;
-			
-			// Inserts the entity into the map, with its u64 id value as the key
+			/* Inserts the entity into the map, with its u64 id value as the key */
 			regions[region_index].insert( entity.id(), entity );
 		}
 	});
@@ -72,22 +67,20 @@ pub fn boundary_system< V: Store<Vec2>, P: Store<Vec2>, A: Store<Attributes> >( 
 																				vel_store: &mut V,
 																				pos_store: &mut P,
 																				atr_store: &A ) {
-	// Converts screen_size tuple to f32 for use in calculations
+	/* Converts screen_size tuple to f32 for use in calculations */
 	let screen_size = ( screen_size.0 as f32, screen_size.1 as f32 );
 	
-	// Applies a closure to each entity with a pos component
+	/* Applies a closure to each entity with a pos component */
 	pos_store.for_each_mut( | entity, pos | {
 		
-		// Gets current entities attributes component
+		/* Gets current entities attributes component */
 		let atr = atr_store.get( entity ).unwrap();
-		
-		// If current entity has a velocity component, assign it to vel then run the following code block
+		/* If current entity has a velocity component, assign it to vel then run the following code block */
 		if let Some( vel ) = vel_store.get_mut( entity ) {
 			
-			// Constrain entities position to within the screen, factoring in the size of the entity
+			/* Constrain entities position to within the screen, factoring in the size of the entity */
 			pos.constrain( &((atr.radius)..(screen_size.0 - atr.radius)), &((atr.radius)..(screen_size.1 - atr.radius)) );
-			
-			// If entity hits the edges, invert their velocity and reduce it based on mass, then reduce it based on surface friction
+			/* If entity hits the edges, invert their velocity and reduce it based on mass, then reduce it based on surface friction */
 			if pos.x - atr.radius < 0.00000001 { vel.x *= -1.0 / atr.mass; vel.y *= 0.99; }
 			if pos.y - atr.radius < 0.00000001 { vel.y *= -1.0 / atr.mass; vel.x *= 0.99; }
 			if pos.x > screen_size.0 - atr.radius - 0.0001 { vel.x *= -1.0 / atr.mass; vel.y *= 0.99; }
@@ -107,22 +100,19 @@ pub fn boundary_system< V: Store<Vec2>, P: Store<Vec2>, A: Store<Attributes> >( 
 ///
 pub fn acceleration_system< A: Store<Vec2>, V: Store<Vec2> >(acc_store: &mut A, vel_store: &mut V ) {
 	
-	// Defines a closure, taking a Entity and a mutable reference of a Vec2, which adds the acc to the vel
+	/* Defines a closure, taking a Entity and a mutable reference of a Vec2, which adds the acc to the vel */
 	let apply_force = | entity: Entity, vel: &mut Vec2 | {
-		
-		// If current entity has a acceleration component, assign it to acc and run the following code block
+		/* If current entity has a acceleration component, assign it to acc and run the following code block */
 		if let Some( acc) = acc_store.get_mut( entity ) {
 			
-			// Adds acc to vel, then zeroes out the acceleration
+			/* Adds acc to vel, then zeroes out the acceleration */
 			vel.add(acc );
-			
-			// Zero out the acceleration afterwards
+			/* Zero out the acceleration afterwards */
 			acc.x = 0.0;
 			acc.y = 0.0;
 		}
 	};
-	
-	// Iterates through every entity with vel components and adds their acc to it using the closure above
+	/* Iterates through every entity with vel components and adds their acc to it using the closure above */
 	vel_store.for_each_mut( apply_force );
 }
 
@@ -138,49 +128,46 @@ pub fn collision_system< V: Store<Vec2>, P: Store<Vec2>, AT: Store<Attributes> >
 																				 pos_store: &P,
 																				 atr_store: &mut AT,
 																				 regions: &mut Vec<HashMap<u64,Entity>>) {
-	// Create a vector to store entities which collide with each other
+	/* Create a vector to store entities which collide with each other */
 	let mut collisions = Vec::new( );
 	
-	// Apply a closure to each entity with a pos component
+	/* Apply a closure to each entity with a pos component */
 	pos_store.for_each( | entity_a, pos_a| {
 		
-		// Get current entity A's attributes
+		/* Get current entity A's attributes */
 		let atr_a = atr_store.get( entity_a ).unwrap();
-		
-		// Apply a closure to each entity in the same region as entity A
+		/* Apply a closure to each entity in the same region as entity A */
 		regions[atr_a.region].iter().for_each( | entity_b | {
 			
-			// Get the pos of another entity, entity B, in entity A's region
+			/* Get the pos of another entity, entity B, in entity A's region */
 			let pos_b = pos_store.get( *entity_b.1 ).unwrap();
-			
-			// Calculate the combined radius of entity A and entity B
+			/* Calculate the combined radius of entity A and entity B */
 			let rad = atr_a.radius + atr_store.get( *entity_b.1 ).unwrap().radius;
-			
-			// If the distance squared between entity A and B is equal to the combined radius squared,
-			// add entity A and B to the collisions list as a tuple
+			/* If the distance squared between entity A and B is equal to the combined radius squared,
+			   add entity A and B to the collisions list as a tuple */
 			if pos_a.dist_sq( pos_b ).abs() - rad.powf( 2.0 ) < 0.0001 && ( entity_b.1.id() != entity_a.id() ) {
 				collisions.push((entity_a, *entity_b.1));
 			}
 		});
 	});
 	
-	// Iterate through each collision pair tuple in the collisions vector
+	/* Iterate through each collision pair tuple in the collisions vector */
 	for ( entity_a, entity_b ) in collisions {
 		
-		// Create some copies of entity A's and entity B's velocity components
+		/* Create some copies of entity A's and entity B's velocity components */
 		let mut vel_a1 = *vel_store.get( entity_a ).unwrap();
 		let mut vel_b = *vel_store.get( entity_b ).unwrap();
 		let vel_a2 = *vel_store.get( entity_a ).unwrap();
 		
-		// Get the attributes components for the entities, will be used to reduce velocity based on their respective masses
+		/* Get the attributes components for the entities, will be used to reduce velocity based on their respective masses */
 		let _atr_a = atr_store.get( entity_a ).unwrap();
 		let _atr_b = atr_store.get( entity_b ).unwrap();
 		
-		// Swaps the angles of the velocity component of the entities
+		/* Swaps the angles of the velocity component of the entities */
 		vel_a1.set_theta( &vel_b.theta() );
 		vel_b.set_theta( &vel_a2.theta() );
 		
-		// Assigns the swapped velocities back to the entities
+		/* Assigns the swapped velocities back to the entities */
 		*vel_store.get_mut( entity_a ).unwrap() = vel_a1;
 		*vel_store.get_mut( entity_b ).unwrap() = vel_b;
 	}
@@ -198,20 +185,20 @@ pub fn render_system< P: Store<Vec2>, A: Store<Attributes> >(display: &mut Rayli
 															 screen_size: (i32, i32),
 															 pos_store: &P,
 															 atr_store: &A ) {
-	// Converts screen_size tuple to f32, for use in calculations
+	/* Converts screen_size tuple to f32, for use in calculations */
 	let screen_size = ( screen_size.0 as f32, screen_size.1 as f32 );
 	
-	// Applies a closure for each entity with a position
+	/* Applies a closure for each entity with a position */
 	pos_store.for_each( | entity, pos| {
 		
-		// If the current entity has a attributes component, assign it to atr and run the following code block
+		/* If the current entity has a attributes component, assign it to atr and run the following code block */
 		if let Some( atr ) = atr_store.get( entity ) {
 			
-			// Maps the y value, from the range 0->screen_height, to the range screen_height->0, so that the coordinate (0,0) is the bottom left
-			// instead of the top left
+			/* Maps the y value, from the range 0->screen_height, to the range screen_height->0, so that the coordinate (0,0) is the bottom left
+			   instead of the top left */
 			let y = map( pos.y, 0.0..screen_size.1, screen_size.1..0.0 );
 			
-			// Draws a circle at the entities position, with the entities radius and color
+			/* Draws a circle at the entities position, with the entities radius and color */
 			display.draw_circle(pos.x as i32, y as i32, atr.radius as f32, atr.color );
 		}
 	});
@@ -231,20 +218,20 @@ pub fn drop_system< AT: Store<Attributes>, P: Store<Vec2>, V: Store<Vec2>, A: St
 																							vel_store: &mut V,
 																							pos_store: &mut P,
 																							atr_store: &mut AT ) {
-	// Creates a vector of entities which will be dropped
+	/* Creates a vector of entities which will be dropped */
 	let mut to_drop = Vec::new( );
 	
-	// Converts screen_size tuple to f32, for use in calculations
+	/* Converts screen_size tuple to f32, for use in calculations */
 	let screen_size = ( screen_size.0 as f32, screen_size.1 as f32 );
 	
-	// Checks each entity to see if it's position is out of bounds. If it is, the entity is added to the to_drop vector
+	/* Checks each entity to see if it's position is out of bounds. If it is, the entity is added to the to_drop vector */
 	pos_store.for_each( | entity, pos | {
 		if pos.x > screen_size.0 + 2.0 || pos.x < -2.0 || pos.y > screen_size.1 + 2.0 || pos.x < -2.0 {
 			to_drop.push( entity );
 		}
 	});
 
-	// For each entity in the to_drop vector, drop it from the entity manager and each components store
+	/* For each entity in the to_drop vector, drop it from the entity manager and each components store */
 	for mut td in to_drop {
 		entity_manager.drop( &mut td );
 		pos_store.drop( td );
