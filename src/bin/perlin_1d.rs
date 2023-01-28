7e9ebaa26257109c80fd7cf8a39ad2d600d0b5fd
dwclake <devonwebb559@gmail.com>
2023-01-28 15:43:43 -0800

    Added basic 1D perlin noise graphing, using the crate Noise

--- src/bin/perlin_1d.rs
+++ src/bin/perlin_1d.rs
@@ -26,9 +26,8 @@ fn main() {
 	/* Creation of a perlin object, with a random seed */
 	let perlin = Perlin::new(thread_rng().next_u32() );
 	
-	/* Creation of offsets, which are used to get values from the perlin object */
+	/* Creation of the x offset, which is used to get values from the perlin object */
 	let mut x_off = thread_rng().gen_range(0.0..100.0);
-	let mut y_off = thread_rng().gen_range(0.0..100.0);
 	
 	/* Draw
 	   Loops until the user closes the window, place code to run each loop in following while loop */
@@ -39,11 +38,10 @@ fn main() {
 		/* Clears the background and sets it's colour to black */
 		display.clear_background( Color::BLACK );
 		
-		/* Get a value out of perlin, using the current offsets */
-		let val = perlin.get([x_off, y_off]).abs() as f32 * (width - 10.0) + 50.0;
-		/* Increments/decrements offsets by small values to get get smooth randomness*/
+		/* Get a value out of perlin, using the current x offset, with y offset set to 0 */
+		let val = perlin.get([x_off, 0.0]).abs() as f32 * (width - 10.0) + 50.0;
+		/* Increments offset by a small value to get get smooth randomness */
 		x_off += 0.01;
-		y_off -= 0.01;
 		
 		/* Draws a circle with a x pos of the perlin value, and a y pos of 1/2 the screen height */
 		display.draw_circle(val as i32, (height / 2.0) as i32, 10.0, Color::WHITE);
